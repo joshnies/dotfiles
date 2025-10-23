bc2608be2d819e3dc8120de01ef0436dc98b0a98
Joshua Nies <50473759+joshnies@users.noreply.github.com>
2025-10-23 06:46:11 -0400

    split nvim config between linux and macos

---
+++ nvim-macos/.config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,48 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  init = function()
+    -- Replace netrw with neo-tree
+    if vim.fn.argc() == 1 then
+      local arg0 = tostring(vim.fn.argv(0))
+      if vim.fn.isdirectory(arg0) == 1 then
+        require 'neo-tree'
+      end
+    end
+  end,
+  opts = {
+    close_if_last_window = true,
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        never_show = {
+          '.DS_Store',
+          '.git',
+          'thumbs.db',
+        },
+      },
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+    window = {
+      mappings = {
+        ['e'] = 'open',
+      },
+    },
+  },
+}
