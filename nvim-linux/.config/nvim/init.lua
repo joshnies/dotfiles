3e456cd994256a6f272d90f527620d27087780b5
Joshua Nies <50473759+joshnies@users.noreply.github.com>
2025-10-25 11:49:46 -0400

    zed-linux

--- nvim-linux/.config/nvim/init.lua
+++ nvim-linux/.config/nvim/init.lua
@@ -767,8 +767,8 @@ require('lazy').setup({
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
           ['<CR>'] = cmp.mapping.confirm { select = true },
-          ['<Tab>'] = cmp.mapping.select_next_item(),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<C-j>'] = cmp.mapping.select_next_item(),
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
